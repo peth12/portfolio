42afa3967e3c1d0d3f60ae682be413c5ddcd9e2a
peth12 <peth12>
2023-10-23 13:45:05 +0700

    add data

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,10 +8,7 @@ const About = () => {
         <article>
           <SectionTitle text='code and coffee' />
           <p className='text-slate-600 mt-8 leading-loose'>
-            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint
-            maiores itaque doloribus asperiores dolorum quas voluptates
-            distinctio, porro beatae quam provident libero animi totam,
-            praesentium voluptatem corrupti dignissimos ipsum accusantium!
+
           </p>
         </article>
       </div>
