4fe3e3b8378dd6cea42d6a93229ab3cb322f481e
peth12 <peth12>
2023-10-23 11:12:46 +0700

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,29 @@
+import heroImg from '../assets/hero.svg';
+import { FaGithubSquare, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
+
+const Hero = () => {
+  return (
+    <div className='bg-emerald-100 py-24'>
+      <div className='align-element grid md:grid-cols-2 items-center gap-8'>
+        <article>
+          <h1 className='text-7xl font-bold tracking-wider'>I'm Peth</h1>
+          <p className='mt-4 text-3xl text-slate-700 capitalize tracking-wide'>
+            Full-Stack Developer
+          </p>
+          <div className='flex gap-x-4 mt-4'>
+            <a href='https://github.com/peth12' target='_blank' rel="noreferrer">
+              <FaGithubSquare className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+            </a>
+            <a href='https://www.linkedin.com/in/yodpeth-pimarthan/' target='_blank' rel="noreferrer">
+              <FaLinkedin className='h-8 w-8 text-slate-500 hover:text-black duration-300' />
+            </a>
+          </div>
+        </article>
+        <article className='hidden md:block'>
+          <img src={heroImg} className='h-80 lg:h-96' />
+        </article>
+      </div>
+    </div>
+  );
+};
+export default Hero;
